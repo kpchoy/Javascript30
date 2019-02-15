5d38dce5ebb6ad53d115099d1a81cc8ac32741bb
Kevin Choy <kevinchoy22@gmail.com>
2019-02-15 10:49:27 -0800

    time update function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay () {
   if (video.paused) {
@@ -24,10 +24,23 @@ function skip () {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate () {
+  video[this.name] = this.value;
+}
+
+function handleProgress () {
+  const percent = (video.currentTime / video.duration) * 100;
+
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach((button) => button.addEventListener('click', skip));
+ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
