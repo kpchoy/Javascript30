03a90165afdab17ca12b6223d77d552d6ee2978b
Kevin Choy <kevinchoy22@gmail.com>
2019-04-17 12:02:02 -0700

    clear interval once seconds are below 0

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,3 +1,16 @@
-function timer(seconds) {
+let countDown;
 
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  countDown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (seconds < 0) {
+      clearInterval(countDown);
+      return;
+    }
+    console.log(secondsLeft);
+  }, 1000)
 }
