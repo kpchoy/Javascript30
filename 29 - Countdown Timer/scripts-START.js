e3e5646c5269e166bea54ba6eab20f4931db3182
Kevin Choy <kevinchoy22@gmail.com>
2019-04-17 14:31:12 -0700

    each button creates a different countdown time

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
 
+  displayEndTime(then);
   displayTimeLeft(seconds);
 
   countDown = setInterval(() => {
@@ -23,6 +26,25 @@ function displayTimeLeft(seconds) {
   const min = Math.floor(seconds / 60);
   const remainderSec = seconds % 60;
   const display = `${min}:${remainderSec < 10 ? '0': ''}${remainderSec}`;
+  document.title = display;
   timerDisplay.textContent = display;
 
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+
+  const min = end.getMinutes();
+
+
+
+  endTime.textContent = `Be back at ${hour}:${min}`
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
